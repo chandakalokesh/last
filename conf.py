71cce4360202156fb4d2566c75fdf50b566b1544
chandakalokesh <79193255+chandakalokesh@users.noreply.github.com>
2021-02-18 16:50:20 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+project = 'new project'
+copyright = '2020, Lokesh'
+author = 'Lokesh ch'
 
 # The full version, including alpha/beta/rc tags
 release = ''
